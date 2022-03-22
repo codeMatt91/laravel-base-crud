24fbff04471a1335584f5b03cbaf51ab0a8a34fa
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-22 16:34:28 +0100

    created array in config + header in page + controller

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,8 +1,23 @@
-<nav class="navbar navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAgW9vwPvI4zCA0A-bH7tfOoUQFD3stsL25w&usqp=CAU"
-                alt="Roma Comic festival" width="30" height="24">
-        </a>
-    </div>
-</nav>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">{{ env('APP_NAME') }}</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" aria-current="page"
+                            href="{{ url('/') }}">Home</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-light" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</header>
